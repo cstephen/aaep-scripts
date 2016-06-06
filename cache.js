c793269f1670bd19b3cafd75e0a7d1c3f2114f5a
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-06-06 13:15:52 -0800

    Now using pg-promise's built-in sequential transaction support and closing connection after script finishes.

--- cache.js
+++ cache.js
@@ -7,7 +7,6 @@ var aceDrupal = require('./ace_drupal.js');
 var drupalBaseUrl = 'http://localhost:9090';
 var drupalAgent = new aceDrupal('http://localhost/api');
 
-var db;
 var pgConnection = {
   host: 'localhost',
   port: 5432,
@@ -16,12 +15,14 @@ var pgConnection = {
 };
 
 var items = [];
-var drupalInitPromise = drupalAgent.initialize({
+var drupalResultsPromise = drupalAgent.initialize({
   'username': 'ACE Import User',
   'password': 'password'
-}).then(function() {
+})
+.then(function() {
   return drupalAgent.get('/views/map_data_export.json');
-}).then(function(results) {
+})
+.then(function(results) {
   return new Promise(function(resolve, reject) {
     async.each(results, function(result, callback) {
       var geoJson = JSON.parse(result.geofield);
@@ -61,61 +62,77 @@ var drupalInitPromise = drupalAgent.initialize({
     });
   });
 })
-.then(function() {
-  db = pgp(pgConnection);
-  return db.none('BEGIN')
-})
-.then(function() {
-  return db.none('DROP TABLE IF EXISTS aaep_cache_new');
-})
-.then(function() {
-  return db.none('DROP INDEX IF EXISTS theme_idx_new');
-})
-.then(function() {
-  return db.none('CREATE TABLE aaep_cache_new (' +
-    'id integer,' +
-    'title character varying(255),' +
-    'snippet text,' +
-    'link character varying(2048),' +
-    'url character varying(2048),' +
-    'lat double precision,' +
-    'lng double precision,'+
-    'geometry_type text,' +
-    'geometry text,' +
-    'ordinal bigint,' +
-    'theme character varying(255),' +
-    'image character varying(2048)' +
-  ')');
-})
-.then(function() {
-  return db.none('CREATE INDEX theme_idx_new ON aaep_cache_new (theme)');
-})
-.then(function() {
-  return db.none('COMMIT');
-})
-.then(function() {
-  return db.none('BEGIN');
+
+
+var db = pgp(pgConnection);
+
+function createTable(index, data, delay) {
+    switch (index) {
+        case 0:
+            return this.query('DROP TABLE IF EXISTS aaep_cache_new');
+        case 1:
+            return this.query('DROP INDEX IF EXISTS theme_idx_new');
+        case 2:
+            return this.query('CREATE TABLE aaep_cache_new (' +
+                              '  id integer,' +
+                              '  title character varying(255),' +
+                              '  snippet text,' +
+                              '  link character varying(2048),' +
+                              '  url character varying(2048),' +
+                              '  lat double precision,' +
+                              '  lng double precision,'+
+                              '  geometry_type text,' +
+                              '  geometry text,' +
+                              '  ordinal bigint,' +
+                              '  theme character varying(255),' +
+                              '  image character varying(2048)' +
+                              ')');
+        case 3:
+          return this.query('CREATE INDEX theme_idx_new ON aaep_cache_new (theme)');
+        case 4:
+          return this.query('GRANT SELECT ON aaep_cache_new TO scott');
+    }
+}
+
+function swapTable(index, data, delay) {
+    switch (index) {
+        case 0:
+            return this.query('DROP TABLE IF EXISTS aaep_cache');
+        case 1:
+            return this.query('ALTER TABLE aaep_cache_new RENAME TO aaep_cache');
+        case 2:
+            return this.query('ALTER INDEX theme_idx_new RENAME TO theme_idx');
+    }
+}
+
+drupalResultsPromise
+.then(function(results) {
+  return db.tx(function(t) {
+    return this.sequence(createTable);
+  });
 })
 .then(function() {
   return new Promise(function(resolve, reject) {
     async.eachSeries(items, function(item, callback) {
       var query = 'INSERT INTO aaep_cache_new VALUES (' +
-                    '${id},' +
-                    '${title},' +
-                    '${snippet},' +
-                    '${link},' +
-                    '${url},' +
-                    '${lat},' +
-                    '${lng},' +
-                    '${geometry_type},' +
-                    '${geometry},' +
-                    '${ordinal},' +
-                    '${theme},' +
-                    '${image}' +
+                  '  ${id},' +
+                  '  ${title},' +
+                  '  ${snippet},' +
+                  '  ${link},' +
+                  '  ${url},' +
+                  '  ${lat},' +
+                  '  ${lng},' +
+                  '  ${geometry_type},' +
+                  '  ${geometry},' +
+                  '  ${ordinal},' +
+                  '  ${theme},' +
+                  '  ${image}' +
                   ')';
-      db.none(query, item).then(function() {
+      db.none(query, item)
+      .then(function() {
         callback();
-      }).catch(function(error) {
+      })
+      .catch(function(error) {
         callback(error);
         reject(error);
       });
@@ -128,32 +145,19 @@ var drupalInitPromise = drupalAgent.initialize({
   });
 })
 .then(function() {
-  return db.none('GRANT SELECT ON aaep_cache_new TO scott');
-})
-.then(function() {
-  return db.none('COMMIT');
-})
-.then(function() {
-  return db.none('BEGIN');
-})
-.then(function() {
   return db.one('SELECT COUNT(*) FROM aaep_cache_new');
 })
 .then(function(result) {
   if(result.count != items.length) {
     throw new Error(items.length + ' expected in database, but found only ' + result.count);
   }
-  return db.none('DROP TABLE aaep_cache');
+  return db.tx(function(t) {
+    return this.sequence(swapTable);
+  });
 })
 .then(function() {
-  return db.none('ALTER TABLE aaep_cache_new RENAME TO aaep_cache');
+  pgp.end();
 })
-.then(function() {
-  return db.none('ALTER INDEX theme_idx_new RENAME TO theme_idx');
-})
-.then(function() {
-  db.none('COMMIT');
-})
-.catch(function(error) {
-  console.log(error);
+.catch(function(err) {
+  console.log(err);
 });
